dc3625b98d6228e75ee75559e72ed1d1149eb014
Colin Millar <colin.millar@ices.dk>
2021-07-01 10:05:18 +0200

    remove partial f scripts

--- data.R
+++ data.R
@@ -3,8 +3,4 @@
 ## Before:
 ## After:
 
-source("data_stocklist.R")
 source("data_f-at-age.R")
-
-source("data_intercatch.R")
-source("data_fleet_categories.R")
--- output.R
+++ output.R
@@ -7,6 +7,5 @@ library(icesTAF)
 
 mkdir("output")
 
-source("output_partial_fatage.R")
 source("output_upload.R")
 source("output_download.R")
